41c97a2af46e22394ac6dfb2e71962ba8517e834
Deeksha20049 <deeksha20049@iiitd.ac.in>
2022-11-07 15:13:06 +0530

    some fix

--- pages/api/user/uploadSet.js
+++ pages/api/user/uploadSet.js
@@ -1,15 +1,10 @@
-import { create } from "../../../helpers/api/files-data";
+// import { create } from "../../../helpers/api/files-data";
 import { ahandler } from "../../../helpers/api/ahandler";
-import { userService } from "services";
+// import { userService } from "services";
 import fs from 'fs';
 let files = JSON.parse(fs.readFileSync('data/tempfiles.json'));
 
 const handler = async (req, res) => {
-    // const fileType = localStorage.getItem("fileType");
-    // const user = userService.userValue;
-    // console.log("fileType", fileType);
-    // console.log("user", user);
-    // console.log("req.body", req.body);
     const body = JSON.parse(req.body);
     const fileType = body['fileType'];
     const owner = body['owner'];
